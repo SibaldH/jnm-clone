f68284410f7201272ca1086cc63866d4c88eef0f
SibaldH <Sibald.hulselmans@protonmail.com>
2024-01-31 13:55:12 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Getting Started
+
+1. Create a new project based on my template
+
+```
+npx create-next-app --example https://github.com/SibaldH/mystack [PROJECT_NAME]
+```
+
+2. run the server:
+
+```
+cd [PROJECT_NAME]
+npm run dev
+```
+
+3. Open [http://localhost:3000](http://localhost:3000).
+
+# My web programming stack of 2024
+
+This programming stack contains:
+
+- [nextjs](https://nextjs.org)
+- [tailwind](https://tailwindcss.com)
+- [typescript](https://www.typescriptlang.org)
+- [supabase](https://supabase.com)
+- [shadcn-ui](https://ui.shadcn.com)
+- [zod](https://zod.dev)
+- [radix-ui/icons](https://radix-ui.com/icons)
---
+++ src/app/auth/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { KeyRound } from "lucide-react";
+import { FaGithub } from "react-icons/fa6";
+import { FaGoogle } from "react-icons/fa";
+import { supabaseBrowser } from "@/lib/supabase/browser";
+import { useSearchParams } from "next/navigation";
+
+export default function AuthPage() {
+  const params = useSearchParams();
+  const next = params.get("next");
+  const handleLoginOAuth = (provider: "github" | "google") => {
+    const supabase = supabaseBrowser();
+    if (next) {
+      supabase.auth.signInWithOAuth({
+        provider,
+        options: {
+          redirectTo: location.origin + "/auth/callback?next=" + next,
+        },
+      });
+    } else {
+      supabase.auth.signInWithOAuth({
+        provider,
+        options: { redirectTo: location.origin + "/auth/callback" },
+      });
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <div className="w-96 rounded-md border p-5 space-y-5 bg-slate-900">
+        <div className="flex items-center gap-2">
+          <KeyRound />
+          <h1 className="text-2xl font-bold">Next + Supabase</h1>
+        </div>
+        <p className="text-sm text-gray-300">Register/SignIn today</p>
+        <div className="flex flex-col gap-5">
+          <Button
+            className="w-full flex items-center gap-2"
+            variant="outline"
+            onClick={() => handleLoginOAuth("github")}
+          >
+            <FaGithub />
+            Github
+          </Button>
+          <Button
+            className="w-full flex items-center gap-2"
+            variant="outline"
+            onClick={() => handleLoginOAuth("google")}
+          >
+            <FaGoogle />
+            Google
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
